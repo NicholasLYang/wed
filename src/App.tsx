0272a41ee7a0c47c56694e6ba3c698a5fcfb03fb
Nicholas Yang <ny585@nyu.edu>
2020-04-20 02:18:00 -0400

    Using a server rn to get the file and load it. Will change to completely client side eventually.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import React, {useState} from 'react';
-import './App.css';
-import {parse} from "./parser";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { parse } from "./parser";
+import { hexy } from "hexy";
 
 function App() {
   const [inputText, setInputText] = useState("");
   const [code, setCode] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:2000/")
+      .then((res) => res.json())
+      .then((body) => {
+        setCode(body.code);
+      });
+  }, []);
   return (
     <div className="App">
-        <p>
-          {code}
-        </p>
-      <form onSubmit={() => {
-        setCode(inputText);
-      }}>
-        <input value={inputText} onChange={(e) => setInputText(e.currentTarget.value)} />
+      <p>{code}</p>
+      <form
+        onSubmit={() => {
+          setCode(inputText);
+        }}
+      >
+        <input
+          value={inputText}
+          onChange={(e) => setInputText(hexy(e.currentTarget.value))}
+        />
         <button type="submit"> Submit </button>
       </form>
     </div>
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import {Request, Response} from "express";
 
 const express = require("express");
+const { hexy } = require("hexy");
+const fs = require("fs").promises;
 const app = express();
+const cors = require("cors");
 const port = 2000;
 
-app.get("/read", (req: Request, res: Response) => {
-  req.params
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  fs.readFile("out.wasm").then((data: Buffer) => {
+    res.json({  code: hexy(data, { numbering: "none", annotate: "none"}) })
+  });
 });
 
-app.post("/write", (req: Request, res: Response) => {
-
-})
-
-
-
+app.listen(port, () => console.log("listening on port " + port));
 export {}
